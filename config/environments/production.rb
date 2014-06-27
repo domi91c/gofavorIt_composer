51befb6048dd0d5d7d50a2ee7d5928c5903cd65c
portOdin <dominic.n@me.com>
2014-06-26 21:44:14 -0400

    auto Heroku commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
     password: "element9"
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = { :host => "http://stark-stream-4511.herokuapp.com/" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
