63c086f245a612b37c35bcc526e2d2350cd342f0
portOdin <dominic.n@me.com>
2014-06-26 21:48:49 -0400

    auto Heroku commit

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+config.action_mailer.default_url_options = { :host => 'http://stark-stream-4511.herokuapp.com/' }
+
 # Initialize the Rails application.
 Rails.application.initialize!
